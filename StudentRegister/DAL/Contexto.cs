82af2e3ec878343878bd3f1235a75e32d7803fad
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-01-19 10:05:50 -0400

    Add project files.

---
+++ StudentRegister/DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentRegister.Models;
+
+namespace StudentRegister.DAL
+{
+    public class Contexto: DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options): base(options) { }
+
+        public DbSet<Estudiantes> Estudiantes { get; set; }
+    }
+}
