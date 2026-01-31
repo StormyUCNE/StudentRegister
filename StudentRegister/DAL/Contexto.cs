d0c356f53e4d7c74763352f45700ff8d29469219
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-01-30 20:17:51 -0400

    Agrega el DbSet para el modelo TiposPuntos en el Contexto.cs

--- StudentRegister/DAL/Contexto.cs
+++ StudentRegister/DAL/Contexto.cs
@@ -6,4 +6,5 @@ public class Contexto : DbContext
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
 
     public DbSet<Estudiantes> Estudiantes { get; set; }
+    public DbSet<TiposPuntos> TiposPuntos { get; set; }
 }
