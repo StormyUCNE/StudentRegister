b5dce40258f37d9654a445e60ffae039deb7a9b7
StormyUCNE <stormy_silverio@ucne.edu.do>
2026-01-31 01:20:51 -0400

    Agrega la migración para añadir el nuevo modelo de TiposPuntos como tabla en la BBDD.

--- StudentRegister/Migrations/20260131023503_TiposPuntos.Designer.cs
+++ StudentRegister/Migrations/20260131023503_TiposPuntos.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentRegister.DAL;
 
@@ -10,9 +11,11 @@ using StudentRegister.DAL;
 namespace StudentRegister.Migrations
 {
     [DbContext(typeof(Contexto))]
-    partial class ContextoModelSnapshot : ModelSnapshot
+    [Migration("20260131023503_TiposPuntos")]
+    partial class TiposPuntos
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
